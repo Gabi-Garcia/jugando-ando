c3ae7b05b107f666456ae5778d1c4356f2766749
Gabi-Garcia <96738373+Gabi-Garcia@users.noreply.github.com>
2024-02-20 16:25:28 +0100

    Modified: media querys, styles

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -13,25 +13,27 @@ const About = () => {
     <>
       <div className='caja'> 
         <div className='about'>
-          <h2>
-            Te damos la bienvenida a nuestro espacio de juegos y aprendizaje
-          </h2>
-          <p className='textoAbout'>
-          Nuestra página web de juegos tiene algo para todos. ¡Así que adelante,
-          explora y disfruta de la experiencia de juego que hemos preparado para ti!
-            ¿Eres fanático del clásico Tres en Raya? Desafía a tus amigos en este juego de estrategia que nunca 
-            pasa de moda.
-            Si amas la música, nuestro juego de adivinanzas de palabras te llevará a un viaje melódico.
-            Pon a prueba tus conocimientos y adivina palabras aleatorias relacionadas con ese fascinante mundo
-            Y para los amantes de los desafíos lógicos, ofrecemos el clásico Sudoku. Pon a prueba tu mente
-            y desarrolla tus habilidades de 
-            resolución de problemas mientras te sumerges en este juego numérico fascinante.
-          </p>
+                <h2>
+                  Te damos la bienvenida a nuestro espacio de juegos y aprendizaje
+                </h2>
+                <p className='textoAbout'>
+                Nuestra página web de juegos tiene algo para todos. ¡Así que adelante,
+                explora y disfruta de la experiencia de juego que hemos preparado para ti!
+                  ¿Eres fanático del clásico Tres en Raya? Desafía a tus amigos en este juego de estrategia que nunca 
+                  pasa de moda.
+                  Si amas la música, nuestro juego de adivinanzas de palabras te llevará a un viaje melódico.
+                  Pon a prueba tus conocimientos y adivina palabras aleatorias relacionadas con ese fascinante mundo
+                  Y para los amantes de los desafíos lógicos, ofrecemos el clásico Sudoku. Pon a prueba tu mente
+                  y desarrolla tus habilidades de 
+                  resolución de problemas mientras te sumerges en este juego numérico fascinante.
+                </p>
         </div>
-        <MyButton><Link to="/Home">IR JUEGOS</Link></MyButton>
-      <div className='footerAbout'>
-      <Footer />
-      </div>
+            <div className='aboutBoton'>
+            <MyButton><Link to="/Home">IR JUEGOS</Link></MyButton>
+            </div>
+            <div className='footerAbout'>
+            <Footer />
+            </div>
       </div>
     </>
   )
